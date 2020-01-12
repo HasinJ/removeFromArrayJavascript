785450d1b31a3297ba925d0ef7928ec79bcc715e
HasinJ <hasinchoudhuryj@gmail.com>
2020-01-12 16:17:35 -0500

    all tests working

--- removeFromArray.js
+++ removeFromArray.js
@@ -1,10 +1,13 @@
-const removeFromArray = function(array,remove) {
-  for (let i = 1; i < array.length; i++) {
-    if (array[i]==remove) {
-      array.splice(i,1);
+const removeFromArray = function(array) {
+  for (let j = 1; j < arguments.length; j++) { //counter variable starts from 1 instead of 0 because argument[0] is an array
+    let remove=undefined; //resets variable
+    remove = arguments[j];
+
+    for (let i = 0; i < array.length; i++) {
+      if (array[i]==remove) { array.splice(i,1);}
     }
   }
-  return array;
+    return array;
 }
 
 module.exports = removeFromArray
