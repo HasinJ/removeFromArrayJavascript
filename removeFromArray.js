e40c05fa7a03872d94e3f5756735f5dec9969771
HasinJ <hasinchoudhuryj@gmail.com>
2020-01-12 15:57:43 -0500

    first test working

--- removeFromArray.js
+++ removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array,remove) {
+  for (let i = 1; i < array.length; i++) {
+    if (array[i]==remove) {
+      array.splice(i,1);
+    }
+  }
+  return array;
 }
 
 module.exports = removeFromArray
